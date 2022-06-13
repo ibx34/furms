f136476695c667f5144ab51a99d613a26d611acb
ibx34 <guamjust@gmail.com>
2022-06-12 23:12:02 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-migrate/migrate/v4"
+	PgxMigration "github.com/golang-migrate/migrate/v4/database/pgx"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jackc/pgx/v4"
+	"github.com/joho/godotenv"
+)
+
+// CREATE TABLE forms (form_id BIGSERIAL PRIMARY KEY);
+type Form struct {
+	FormId       uint64 `db:"form_id" json:"form_id"`
+	FormName     string `db:"name" json:"name"`
+	FormPassword string `db:"password" json:"password"`
+}
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+type App struct {
+	Context  context.Context
+	Database *pgx.Conn
+}
+
+var (
+	UsingDollarSigns = sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	ctx := context.Background()
+
+	// urlExample := "postgres://username:password@localhost:5432/database_name"
+	conn, err := pgx.Connect(ctx, os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	instance, errOpen := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	if errOpen != nil {
+		fmt.Println("Failed to open database for migration")
+	}
+	if errPing := instance.Ping(); errPing != nil {
+		fmt.Println("Cannot migrate, failed to connect to target server")
+	}
+
+	driver, _ := PgxMigration.WithInstance(instance, &PgxMigration.Config{
+		MigrationsTable:       "_migration",
+		SchemaName:            "public",
+		StatementTimeout:      60 * time.Second,
+		MultiStatementEnabled: true,
+	})
+
+	migrate, err := migrate.NewWithDatabaseInstance(
+		"file://./migrations",
+		"pgx", driver,
+	)
+
+	if err != nil {
+		fmt.Println("Failed to create migration")
+	}
+	migrate.Up()
+
+	defer conn.Close(ctx)
+
+	app_context := App{Context: ctx, Database: conn}
+	r := gin.Default()
+	r.POST("/forms/new", app_context.create_form)
+	r.GET("/forms/:form_id", app_context.get_form)
+	r.Run()
+}
+
+// This function is cute. It will check for a password Nya.
+func (app_context *App) get_form(c *gin.Context) {
+	form_id := c.Params.ByName("form_id")
+	log.Println(form_id)
+	form_password := c.Request.Header.Get("X-Password")
+
+	if len(form_id) == 0 {
+		return
+	}
+
+	query, args, errQueryArgs := UsingDollarSigns.Select("*").From("forms").Where(sq.Eq{"form_id": form_id}).ToSql()
+	if errQueryArgs != nil {
+		fmt.Println(errQueryArgs)
+		return
+	}
+	form := Form{}
+	err := app_context.Database.QueryRow(app_context.Context, query, args...).Scan(&form.FormId, &form.FormName, &form.FormPassword)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	}
+
+	if len(form.FormPassword) != 0 {
+		if len(form_password) == 0 {
+			c.JSON(http.StatusUnauthorized, Response{
+				Message: "Please remake your request with a password.",
+			})
+			return
+		}
+
+		if form_password != form.FormPassword {
+			c.JSON(http.StatusForbidden, Response{
+				Message: "Password did not match that of the requested form.",
+			})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, form)
+}
+
+func (app_context *App) create_form(c *gin.Context) {
+	type CreateFormRequest struct {
+		FormName     string `json:"name"`
+		FormPassword string `json:"password"`
+	}
+
+	var new_form CreateFormRequest
+	if err := c.BindJSON(&new_form); err != nil {
+		return
+	}
+
+	query, args, errQueryArgs := UsingDollarSigns.Insert("forms").
+		Columns("name", "password").
+		Values(new_form.FormName, new_form.FormPassword).
+		Suffix("RETURNING *").
+		ToSql()
+
+	if errQueryArgs != nil {
+		fmt.Println(errQueryArgs)
+		return
+	}
+
+	fmt.Println(query)
+	fmt.Println(args)
+
+	form := Form{}
+	err := app_context.Database.QueryRow(app_context.Context, query, args...).Scan(&form.FormId, &form.FormName, &form.FormPassword)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	}
+	fmt.Println(form)
+}
---
+++ migrations/20220613020735_create_forms.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS forms (
+    form_id     BIGSERIAL PRIMARY KEY,
+    name        VARCHAR,
+    password    VARCHAR
+)
