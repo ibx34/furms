0e57e344e8dc63f984b2cf40c94f273c524b1ce9
ibx34 <guamjust@gmail.com>
2022-07-12 12:08:31 -0400

    oop basic rewrite moment

---
+++ src/config.rs
@@ -0,0 +1,21 @@
+use std::{fs::read_to_string,env};
+use once_cell::sync::Lazy;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub database: String,
+}
+
+pub static CONFIG: Lazy<Config> = Lazy::new(|| {
+    dotenv::dotenv().ok();
+
+    let current_path = env::current_dir().expect("Failed to get current working directory");
+
+    let contents =
+        read_to_string(current_path.as_path().join("config.yaml")).expect("Failed to open your config.yaml");
+    let config =  serde_yaml::from_str::<Config>(&contents)
+        .expect("Failed to parse config.json");
+
+    config
+});
